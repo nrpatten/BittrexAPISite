276a15e04e1ff722ccca3c29e5ed76f89a336865
nrpatten <nrpatten@gmail.com>
2015-06-17 23:50:29 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# BittrexAPISite 
+-------------
+
+* Simple Bittrex API website made with jquery, dataTables and bootstrap
+ * Pulls all of the Bittrex Coin data and displays it in a table
+ * Auto refresh the data every 5 minutes
+ * Bootstrap Style with Pagination ans Search
+ * Print or Copy and Download as CSV, Excel, PDF
---
+++ js/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	var refreshTable = $("#bittrex").DataTable( {
+		"ajax": {
+			"url": "http://162.243.239.186:8000/?url=https://bittrex.com/api/v1.1/public/getmarketsummaries",
+			"dataSrc": "result"
+		},
+		"columns": [
+			{ "data": "MarketName", "render": function (data,type,row) { return '<img src="img/'+ (data).replace('BTC-', '').toLowerCase() +'.png" height="16" width="16"></img> ' + (data) +''; } },
+			{ "data": "Last", "render": function (data,type,row) { return (data).toFixed(8); } },
+                        { "data": "High", "render": function (data,type,row) { return '<span style="font-size:12px;color:green;" class="glyphicon glyphicon-arrow-up"></span> ' + (data).toFixed(8) +''; }  },
+                        { "data": "Low", "render": function (data,type,row) { return '<span style="font-size:12px;color:red;" class="glyphicon glyphicon-arrow-down"></span> ' + (data).toFixed(8) +''; }  },
+			{ "data": "BaseVolume", "render": function (data,type,row) { return '<span style="font-size:12px;color:orange;" class="glyphicon glyphicon-bitcoin"></span> ' + (data) +''; }  },
+			{ "data": "Volume" }
+		],
+                "dom": 'T<"clear">lfrtip',
+                "tableTools": {
+                    "sSwfPath": "swf/copy_csv_xls_pdf.swf",
+                    "aButtons": [
+                        "copy",
+                        "print",
+                        {
+                            "sExtends":    "collection",
+                            "sButtonText": "Save",
+                            "aButtons":    [ "csv", "xls", "pdf" ]
+                        }
+                    ]
+                }
+	} );
+       setInterval (function test() {
+         refreshTable.ajax.reload( null, false );
+       }, 300000);
+} );
